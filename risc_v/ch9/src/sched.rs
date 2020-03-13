a6c0ff96feb83b33330e1e30eef6f911f6ffd217
Stephen Marz <stephen.marz@utk.edu>
2020-03-13 19:38:31 -0400

    Remove mepc, satp, and pid, we get these from the frame address

--- risc_v/ch9/src/sched.rs
+++ risc_v/ch9/src/sched.rs
@@ -3,24 +3,23 @@
 // Stephen Marz
 // 27 Dec 2019
 
-use crate::{cpu::{build_satp, SatpMode},
-            process::{ProcessState, PROCESS_LIST}};
+use crate::process::{ProcessState, PROCESS_LIST};
 
 pub fn schedule() -> usize {
 	unsafe {
 		if let Some(mut pl) = PROCESS_LIST.take() {
 			pl.rotate_left(1);
 			let mut frame_addr: usize = 0;
-			let mut mepc: usize = 0;
-			let mut satp: usize = 0;
-			let mut pid: usize = 0;
+			// let mut mepc: usize = 0;
+			// let mut satp: usize = 0;
+			// let mut pid: usize = 0;
 			if let Some(prc) = pl.front() {
 				match prc.get_state() {
 					ProcessState::Running => {
 						frame_addr =
 							prc.get_frame_address();
-						satp = prc.get_table_address();
-						pid = prc.get_pid() as usize;
+						// satp = prc.get_table_address();
+						// pid = prc.get_pid() as usize;
 					},
 					ProcessState::Sleeping => {},
 					_ => {},
