2eb8acabd73ca408a0782fa0fcc8098fb4818f66
Stephen Marz <stephen.marz@utk.edu>
2020-04-24 19:55:49 -0400

    Remove syscall_exit, kernel processes will return via RA

--- risc_v/src/test.rs
+++ risc_v/src/test.rs
@@ -7,9 +7,7 @@ pub fn test_block() {
     let buffer = crate::kmem::kmalloc(1024);
     println!("Started test block process, buffer is at {:p}.", buffer);
     unsafe {
-        println!("Before FS read");
         syscall_fs_read(8, 1, buffer, 0, 1024);
-        println!("After FS read");
         for i in 0..32 {
             print!("{:02x}  ", buffer.add(i).read());
             if (i+1) % 16 == 0 {
@@ -20,5 +18,4 @@ pub fn test_block() {
     println!();
     crate::kmem::kfree(buffer);
     println!("Test block finished");
-    syscall_exit();
 }
