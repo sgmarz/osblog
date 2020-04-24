8081863709b0de5b6b88665cc2d72f0349c4f91f
Stephen Marz <stephen.marz@utk.edu>
2020-04-24 18:48:19 -0400

    Fix inits loop. We will use WFI to avoid hogging resources.

--- risc_v/src/process.rs
+++ risc_v/src/process.rs
@@ -157,7 +157,7 @@ fn init_process() {
 				fn make_syscall(sysno: usize, duration: usize) -> usize;
 			}
 			println!("Init is still here :), alright, back to sleep.");
-			for _ in 0..100 {
+			for _ in 0..500 {
 				llvm_asm!("wfi");
 			}
 		}
