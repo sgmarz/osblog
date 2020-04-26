d62d3f3816fd5fe036d584a9686cfd10441cb92b
Stephen Marz <stephen.marz@utk.edu>
2020-04-26 09:26:41 -0400

    Added more rigorous test, fixed bug in ELF loader

--- risc_v/src/userspace/helloworld.cpp
+++ risc_v/src/userspace/helloworld.cpp
@@ -1,18 +1,21 @@
 #include <printf.h>
 #include <syscall.h>
 
-int myarray[1000];
+
+const int SIZE = 1000;
+int myarray[SIZE];
+int another_array[5] = {1, 2, 3, 4, 5};
 
 int main()
 {
 	printf("I'm a C++ program, and I'm running in user space. How about a big, Hello World\n");
 	printf("My array is at 0x%p\n", myarray);
 	printf("I'm going to start crunching some numbers, so gimme a minute.\n");
-	for (int i = 0;i < 1000;i++) {
-		myarray[i] = 0;
+	for (int i = 0;i < SIZE;i++) {
+		myarray[i] = another_array[i % 5];
 	}
 	for (int i = 0;i < 100000000;i++) {
-		myarray[i % 1000] += 1;
+		myarray[i % SIZE] += 1;
 	}
 	printf("Ok, I'm done crunching. Wanna see myarray[0]? It's %d\n", myarray[0]);
 	return 0;
