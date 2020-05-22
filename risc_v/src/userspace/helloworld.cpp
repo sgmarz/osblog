5b89cd891a8f5287b5905cd35d05bf35cbb13080
Stephen Marz <stephen.marz@utk.edu>
2020-05-22 16:29:44 -0400

    See if our new floating point works

--- risc_v/src/userspace/helloworld.cpp
+++ risc_v/src/userspace/helloworld.cpp
@@ -1,7 +1,7 @@
 #include <printf.h>
 
-const int SIZE = 1000;
-int myarray[SIZE];
+const int SIZE = 100000;
+double myarray[SIZE];
 int another_array[5] = {1, 2, 3, 4, 5};
 
 int main()
@@ -13,8 +13,8 @@ int main()
 		myarray[i] = another_array[i % 5];
 	}
 	for (int i = 0;i < 100000000;i++) {
-		myarray[i % SIZE] += 1;
+		myarray[i % SIZE] += 10101.10101;
 	}
-	printf("Ok, I'm done crunching. Wanna see myarray[0]? It's %d\n", myarray[0]);
+	printf("Ok, I'm done crunching. Wanna see myarray[0]? It's %lf\n", myarray[0]);
 	return 0;
 }
