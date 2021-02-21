88ca59d799a01dd91f8dbeefc12583e127a41858
Stephen Marz <stephen.marz@utk.edu>
2021-02-20 21:27:47 -0500

    Fix memcpy

--- risc_v/src/cpu.rs
+++ risc_v/src/cpu.rs
@@ -314,17 +314,8 @@ pub fn get_mtime() -> usize {
 
 /// Copy one data from one memory location to another.
 pub unsafe fn memcpy(dest: *mut u8, src: *const u8, bytes: usize) {
-	let bytes_as_8 = bytes / 8;
-	let dest_as_8 = dest as *mut u64;
-	let src_as_8 = src as *const u64;
-
-	for i in 0..bytes_as_8 {
-		*(dest_as_8.add(i)) = *(src_as_8.add(i));
-	}
-	let bytes_completed = bytes_as_8 * 8;
-	let bytes_remaining = bytes - bytes_completed;
-	for i in bytes_completed..bytes_remaining {
-		*(dest.add(i)) = *(src.add(i));
+	for i in 0..bytes {
+		dest.add(i).write(src.add(i).read());
 	}
 }
 
